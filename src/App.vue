f2d49493914ab0f9dad46e49dc31a00e6fc8b844
JP Aguirre <jp.aguirre90@gmail.com>
2020-07-15 11:41:24 -0400

    form-menu

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <FormMenu/>
+  </div>
+</template>
+
+<script>
+import FormMenu from './components/FormMenu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormMenu
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FormMenu.vue
@@ -0,0 +1,46 @@
+<template>
+    <form @submit="enviar" action="">
+        <h3>Menú</h3>
+            <p>Nombre del plato: <input type="text" v-model="nombre"></p>
+            <p>Precio: $<input type="number" v-model="precio" required min="1000" max="10000"></p>
+            <p>Categoría:
+                <select v-model="categoria">
+                    <option value="entrada">Entrada</option>
+                    <option value="pincipal">Plato Principal</option>
+                    <option value="acomp">Acompañamiento</option>
+                    <option value="postre">Postre</option>
+                </select>
+            </p>
+            <p>Descripción: <textarea v-model="descripcion"></textarea></p>
+            <input type="checkbox" id="checkbox" v-model="checked">
+            <label for="checkbox">¿Esta seguro?</label>
+            <p>Esta seguro nuevamente</p><input type="radio" v-model="pick" v-bind:value="a">
+            <p><input type="submit" value="Añadir Item"></p>
+    </form>
+    
+</template>
+
+<script>
+export default {
+name: 'FormMenu',
+  data() {
+    return {
+      nombre: '',
+      precio: 0,
+      categoria: '',
+      descripcion: '',
+      opciones: []
+    }
+  },
+  methods: {
+    enviar: function(ev) {
+      ev.preventDefault();
+      console.log(this.nombre)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
